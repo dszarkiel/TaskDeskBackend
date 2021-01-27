b4fd75a8e29f2297e6a7f2aee9f94b7456e00a51
dszarkiel <d.szarkiel@gmail.com>
2021-01-27 11:40:41 -0600

    finalized backend

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,10 +4,10 @@ class Api::V1::AuthController < ApplicationController
         user = User.find_by(email: params[:email])
         
         if user && user.authenticate(params[:password])
-            render json: {id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email}
+            render json: user
         else
             render json: {error: " Username or password do not exist."}
-        end
+        end 
     end
 
 end
