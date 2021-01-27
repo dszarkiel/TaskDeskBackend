69d8942ca1505f79103c9c80b79b3ce280c71e32
dszarkiel <d.szarkiel@gmail.com>
2021-01-26 21:10:18 -0600

    login functional

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::AuthController < ApplicationController
 
-    def create
+    def create 
         user = User.find_by(email: params[:email])
-        render json: params[:email]
-        # if user && user.authenticate(params[:password])
-        #     render json: {id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email}
-        # else 
-        #     render json: {error: "Username or password does not exist."}
-        # end
+        
+        if user && user.authenticate(params[:password])
+            render json: {id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email}
+        else
+            render json: {error: " Username or password do not exist."}
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :paintings 
       post '/auth', to: 'auth#create'
 
       get '/current_user', to: 'auth#show'
