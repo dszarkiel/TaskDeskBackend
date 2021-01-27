ca43219794bf5dc90641dbb2cc98d329e5e02e85
dszarkiel <d.szarkiel@gmail.com>
2021-01-26 19:54:59 -0600

    pre auth controller generator

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,7 @@ ActiveRecord::Schema.define(version: 2021_01_23_193920) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 User.destroy_all
 Task.destroy_all
 
-user1 = User.create(first_name:"Dom", last_name:"Szar", email:"dom@gmail.com", username:"domszar", password:"abc")
-user2 = User.create(first_name:"John", last_name:"Doe", email:"john@gmail.com", username:"johndoe", password:"abc")
+user1 = User.create(first_name:"Dom", last_name:"Szar", email:"dom@gmail.com", password:"abc")
+user2 = User.create(first_name:"John", last_name:"Doe", email:"john@gmail.com", password:"abc")
 
 task1 = Task.create(title:"Clean", body:"Clean the basement, bathroom, and bedroom.", complete:false, user_id: user1.id)
 task2 = Task.create(title:"Cook", body:"Need to meal prep for the week.", complete:false, user_id: user1.id)
